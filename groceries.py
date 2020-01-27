d0423ff87b43abe7d610acbc078034781c1702b4
ahj30 <ahj30@georgetown.edu>
2020-01-27 09:11:18 -0500

    Update groceries.py

--- groceries.py
+++ groceries.py
@@ -25,7 +25,105 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
-# pprint(products)
+#for p in products:
+ #  print(p["name"])
 
-# TODO: write some Python code here to produce the desired output
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes. 
+    Example: to_usd(4000.444444) 
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+#breakpoint()
+
+
+print("--------------")
+#print("THERE ARE", len(products), " PRODUCTS:")
+#print("THERE ARE "  + str(len(products)) + " PRODUCTS:")
+print(f"THERE ARE {len(products)} PRODUCTS:")
+
+#print("THERE ARE 20 PRODUCTS:")    
+print("--------------")
+
+#for p in products:
+    #print(" + " + p["name"] + " ($" + str(p["price"]) + ")")
+
+
+
+for p in products:
+    print(" + " + p["name"] + " (" + to_usd(p["price"]) + ")")
+    #print(" + " + p["name"] + " ($" + str(p["price"]) + ")")
+
+departments = []
+
+
+for p in products:
+    #print(p["department"])
+    if p["department"] not in departments: 
+        departments.append(p["department"])
+
+
+
+
+
+
+print("--------------")
+print(f"THERE ARE {len(departments)} DEPARTMENTS")
+print("--------------")
+
+for d in departments:
+    print(" + " + d.title())
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#--------------
+#THERE ARE 20 PRODUCTS:
+#--------------
+# + All-Seasons Salt ($4.99)
+# + Chocolate Fudge Layer Cake ($18.50)
+# + Chocolate Sandwich Cookies ($3.50)
+# + Cut Russet Potatoes Steam N' Mash ($4.25)
+# + Dry Nose Oil ($21.99)
+# + Fresh Scent Dishwasher Cleaner ($4.99)
+# + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+# + Green Chile Anytime Sauce ($7.99)
+# + Light Strawberry Blueberry Yogurt ($6.50)
+# + Mint Chocolate Flavored Syrup ($4.50)
+# + Overnight Diapers Size 6 ($25.50)
+# + Peach Mango Juice ($1.99)
+# + Pizza For One Suprema Frozen Pizza ($12.50)
+# + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+# + Pure Coconut Water With Orange ($3.50)
+# + Rendered Duck Fat ($9.99)
+# + Robust Golden Unsweetened Oolong Tea ($2.49)
+# + Saline Nasal Mist ($16.00)
+# + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+# + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+#--------------
+#THERE ARE 10 DEPARTMENTS:
+#--------------
+# + Babies (1 product)
+# + Beverages (5 products)
+# + Dairy Eggs (1 product)
+# + Dry Goods Pasta (1 product)
+# + Frozen (4 products)
+# + Household (1 product)
+# + Meat Seafood (1 product)
+# + Pantry (2 products)
+# + Personal Care (2 products)
+# + Snacks (2 products)
+
+
+
